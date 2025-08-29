26f2e934f1217f310bf8d8708913681fd5988446
Sibtain Iqbal <sibtainiqbal8844@gmail.com>
2025-08-29 11:27:22 -0700

    Folder structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ return(
+  <>
+  
+  <h1 className='text-4xl text-center text-bold text-green-300 mt-8'>Student Mentor</h1>
+  
+  </>
+ )
+}
+
+export default App
